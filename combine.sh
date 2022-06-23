0b594cff516d112662ed7e42f40cdc8404ec5e01
aaryanchhabra <aryanchhabra28@gmail.com>
2022-06-24 02:03:25 +0530

    update

---
+++ combine.sh
@@ -0,0 +1,3 @@
+mkdir SpliSER_combine
+
+python SpliSER_v0.1.7.py combine -S SampleFiles.tsv -O SpliSER_combine/col_VS_tcp23410
--- process.sh
+++ process.sh
@@ -1,7 +1,7 @@
 #bam files in BAM_files
 
-mkdir spliSER_process
+mkdir SpliSER_process
 
 cat SRR_Acc_List.txt | parallel -j 2 "regtools junctions extract -s 0 -o BED_Files/{}.bed BAM_Files/{}.bam"
 
-cat SRR_Acc_LIst.txt | parallel -j 1 "python SpliSER-master/SpliSER_v0.1.7.py process -A TAIR10.1 -B BAM_Files/{}.bam -b BED_Files/{}.bed -o spliSER_process/{}.SpliSER.bed"
+cat SRR_Acc_LIst.txt | parallel -j 1 "python SpliSER-master/SpliSER_v0.1.7.py process -A  -B BAM_Files/{}.bam -b BED_Files/{}.bed -o spliSER_process/{}.SpliSER.bed"
