48a23db7d0e7f365f744b779e192b3c0749c28cc
aaryanchhabra <aryanchhabra28@gmail.com>
2022-06-24 16:29:52 +0530

    update

--- SpliSER_combine.sh
+++ SpliSER_combine.sh
@@ -1,3 +1,3 @@
 mkdir SpliSER_combine
 
-python SpliSER_v0.1.7.py combine -S SampleFiles.tsv -O SpliSER_combine/col_VS_tcp23410
+python SpliSER-master/SpliSER_v0.1.7.py combine -S SamplesFile.tsv -o SpliSER_combine/col_VS_tcp23410
---
+++ SpliSER_output.sh
@@ -0,0 +1,7 @@
+mkdir SpliSER_output_7
+
+python SpliSER-master/SpliSER_v0.1.7.py output -S SamplesFile.tsv -C SpliSER_combine/col_VS_tcp23410.tsv -t DiffSpliSER -o SpliSER_output_7/col_VS_tcp23410 -r 7
+
+mkdir SpliSER_output_10
+
+python SpliSER-master/SpliSER_v0.1.7.py output -S SamplesFile.tsv -C SpliSER_combine/col_VS_tcp23410.tsv -t DiffSpliSER -o SpliSER_output_10/col_VS_tcp23410
